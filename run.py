e1e24fa795031b6c9eef9bf62158a096469364c4
elSergiusWTF <regiees10@hotmail.com>
2021-02-12 18:08:03 +0100

    1

---
+++ run.py
@@ -0,0 +1,7 @@
+from movements import app
+
+if __name__== '__main__':
+    app.run()
+
+#------------------------ ANOTACIONES-----------------------
+#Este fichero lo utilizamos como punto de entrada para nuestra aplicación. Canal de entrada y salida.
